e6d50b10fb69eb87aff0b0239d8a2eb18882c562
testuser <neo.aoi.takahashi@gmail.com>
2021-07-28 13:40:17 +0900

    ～バックエンド

--- backend/todo_api/settings.py
+++ backend/todo_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+    'corsheaders',
     
     'todos', 
 ]
@@ -57,7 +58,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    
 ]
+
+CORS_ORIGIN_WHITELIST = (
+     'http://localhost:3000',
+     'http://127.0.0.1:3000',
+)
+
 
 ROOT_URLCONF = 'todo_api.urls'
 
